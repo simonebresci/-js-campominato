85a3e924e065463da9ca348aa55d3ef778b698b9
Simone <simone.bresci.05@gmail.com>
2021-01-11 15:52:08 +0100

    refactoring + correzione bug difficoltà

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // Testo es
-
 /*
 Il computer deve generare 16 numeri casuali tra 1 e 100.
 I numeri non possono essere duplicati (tadaaa!)
@@ -17,21 +16,7 @@ con difficoltà 0 => tra 1 e 100
 con difficoltà 1 =>  tra 1 e 80
 con difficoltà 2 => tra 1 e 50
 */
-
-
 // [12][3][4][5][6][4][6] ...
-
-
-// Inizializza variabili di gioco
-var difficolt                               // 1 = facile, 2 = medio, 3 = difficile
-var arrayRandom = [];                       //Array contenente numeri casuali
-arrayRandom.length = 16;                    //Numero elementi array Random
-var minRand, maxRand;             //Valori min e max dei numeri random generati
-var arrayNumGiocati = [];                   //Array numeri già scelti
-var nTentativi = 100 - arrayRandom.length;  //N tentativi prima di perdere
-var punteggio = 0                           //Punteggio
-var sconfitta = false;                      //Sconfitta dell'utente
-
 
 
 // FUNZIONI ********************************************************************
@@ -39,7 +24,7 @@ var sconfitta = false;                      //Sconfitta dell'utente
 function generateRandom(minR, maxR){
   return parseInt((Math.random() * (maxR - minR)) + minR) ;
 }
-// Genera  array di numeri casuali
+// Genera array di numeri casuali in array con lunghezza preimpostata
 function generateArrayRandom(arrayR, minR, maxR){
   var numRandom;
 
@@ -50,6 +35,7 @@ function generateArrayRandom(arrayR, minR, maxR){
       numRandom = generateRandom(minR, maxR);
     }while(searchElementArray(numRandom, arrayR))
 
+    //Inserisci numero generato nell'array
     arrayR[i] = numRandom;
 
   }
@@ -60,6 +46,7 @@ function generateArrayRandom(arrayR, minR, maxR){
 // Cerca elemento in un array
 function searchElementArray(search, arraySearch){
 
+  //Scorri tutto array
   for (var i = 0; i < arraySearch.length; i++) {
     if (arraySearch[i] === search){
       // Elemento trovato - esci
@@ -73,13 +60,23 @@ function searchElementArray(search, arraySearch){
 }
 // Verifica che numero sia compreso fra min e max
 function checkMinMaxInt(num, min, max){
+  // input deve essere numeric
+  if (isNaN(num)){
+    return false;
+  }
+
+  // Trasforma input in intero
   num = parseInt(num);
+
+  // input deve essere compreso fra min e max
   if (num < min || num > max) return false;
 
+  // Numero ok
   return true;
 }
 
 // Funzioni debug
+// Stampa array su console.log
 function DebugPrintArray(lista){
   var stringa = '';
   for (var i = 0; i < lista.length; i++) {
@@ -88,6 +85,21 @@ function DebugPrintArray(lista){
   console.log(stringa);
 }
 // /FUNZIONI *******************************************************************
+
+
+// Inizializza variabili di gioco
+var difficolt                               //Difficoltà di gioco 1 = facile, 2 = medio, 3 = difficile
+var arrayRandom = [];                       //Array contenente numeri casuali
+arrayRandom.length = 16;                    //Numero elementi array Random
+var minRand, maxRand;                       //Valori min e max dei numeri random generati
+var arrayNumGiocati = [];                   //Array numeri già scelti
+var nTentativi = 100 - arrayRandom.length;  //N tentativi prima di perdere
+var punteggio = 0                           //Punteggio
+var sconfitta = false;                      //Sconfitta dell'utente
+
+
+
+
 
 // Righe vuote perchè prompt oscura parte alta dello schermo
 console.log('');
@@ -102,19 +114,22 @@ console.log('Benvenuto in JS Campo Minato')
 // Selezione difficoltà
 do{
   difficolt = parseInt(prompt('Seleziona difficoltà - 1 facile, 2 medio, 3 difficile'));
-} while (difficolt < 1 || difficolt > 3)
+} while (!checkMinMaxInt(difficolt, 1, 3))
 
 // Imposta difficoltà
 minRand = 1;
 switch (difficolt) {
   case 1:
     maxRand = 100;
+    nTentativi = maxRand - arrayRandom.length;
     break;
   case 2:
     maxRand = 80;
+    nTentativi = maxRand - arrayRandom.length;
     break;
   case 3:
     maxRand = 50;
+    nTentativi = maxRand - arrayRandom.length;
     break;
 }
 
@@ -126,12 +141,13 @@ arrayRandom = generateArrayRandom(arrayRandom, minRand, maxRand);
 DebugPrintArray(arrayRandom);
 
 // Gioca un numero
-var numUtente;
+var numUtente;      //Ultimo numero scelto dal giocatore
+var inputNotOk = false;
 while(!sconfitta && (nTentativi > 0)){
 
-
-  var inputNotOk = false;
+  // Scelta numero utente attraverso prompt
   do{
+    // Inizializza valore inputNotOk
     inputNotOk = false;
     numUtente = parseInt(prompt('Scegli un numero, min ' + minRand + ' max ' + maxRand));
 
@@ -156,23 +172,25 @@ while(!sconfitta && (nTentativi > 0)){
   arrayNumGiocati.push(numUtente);
   DebugPrintArray(arrayNumGiocati);
 
-  // Decrementa numero Tentativi
+  // Decrementa numero Tentativi ed incrementa punteggio
   nTentativi--;
   punteggio++;
   console.log('Tentativi rimanenti: ' + nTentativi);
 
   // Controlla se presente in array Random
   sconfitta = searchElementArray(numUtente, arrayRandom);
-  // console.log(sconfitta);
 
 }
 
+// Sconfitta dell'utente
 if (sconfitta){
   console.log('Hai perso!');
 }
 
+// Vincita dell'utente
 if (nTentativi <= 0){
   console.log('Hai vinto!!!')
 }
 
+// Mostra punteggio utente
 console.log('Punteggio:' + punteggio);
